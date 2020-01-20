b06b15746224363deecfd239895b06a79c39bf92
yemiOdetola <yemiotola@gmail.com>
2020-01-20 15:19:45 +0100

    social refix, when unavailable

--- src/app/views/pages/apps/socials/socials-list/socials-list.component.ts
+++ src/app/views/pages/apps/socials/socials-list/socials-list.component.ts
@@ -31,6 +31,10 @@ export class SocialsListComponent implements OnInit, OnDestroy {
 	linkedInCode = '';
 	windowsUrl = '';
 	instaUsername = '';
+	birdAvailable = false;
+	fbkAvailable = false;
+	gramAvailable = false;
+	inAvailable = false;
 	constructor(
 		private authService: AuthService,
 		private socialsService: SocialsService,
@@ -46,7 +50,6 @@ export class SocialsListComponent implements OnInit, OnDestroy {
 		}
 		this.authService.authState.subscribe((user) => {
 			this.user = user;
-			console.log('logged in user', user);
 		});
 		this.getAllSocials();
 		this.loadingSubject.next(false);
@@ -62,6 +65,12 @@ export class SocialsListComponent implements OnInit, OnDestroy {
 				this.loadingSubject.next(false);
 				this.setupSocials = socialss['data'];
 				this.setupSocials.forEach(social => {
+					if (social.type === 'facebook') { this.fbkAvailable = true; }
+					if (social.type === 'instagram') { this.gramAvailable = true; }
+					if (social.type === 'linkedin') { this.inAvailable = true; }
+					if (social.type === 'twitter') { this.birdAvailable = true; }
+
+
 					if (social.type === 'facebook') {
 						localStorage.setItem('fbkID', social.data.app_id);
 					}
@@ -73,108 +82,149 @@ export class SocialsListComponent implements OnInit, OnDestroy {
 		);
 	}
 
-	signInWithFB(): void {
-		this.setupSocials.forEach(social => {
-			if (social.type === 'facebook') {
-				const fbkID = social.data.app_id;
-				FacebookLoginProvider.PROVIDER_ID = fbkID;
-				this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(response => {
-					this.layoutUtilsService.showActionNotification('Facebook signin successful', MessageType.Create, 10000, true, true);
-					localStorage.setItem('facebookDetails', JSON.stringify(response));
-					if (localStorage.getItem('facebookDetails')) {
-						this.fbkDetails = JSON.parse(localStorage.getItem('facebookDetails'));
-					}
-					this.addSocial(this.fbkDetails.name, this.fbkDetails.email, localStorage.getItem('fbkID'));
-				});
-			} else {
+	signInWithFB() {
+		if (this.setupSocials) {
+			if (!this.fbkAvailable) {
 				const message = `Facebook account is not configured`;
 				return this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
 			}
-		});
+			this.setupSocials.forEach(social => {
+				if (social && social.type === 'facebook') {
+					console.log('socials setup valid');
+					const fbkID = social.data.app_id;
+					FacebookLoginProvider.PROVIDER_ID = fbkID;
+					this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(response => {
+						this.layoutUtilsService.showActionNotification('Facebook signin successful', MessageType.Create, 10000, true, true);
+						localStorage.setItem('facebookDetails', JSON.stringify(response));
+						if (localStorage.getItem('facebookDetails')) {
+							this.fbkDetails = JSON.parse(localStorage.getItem('facebookDetails'));
+						}
+						this.addSocial(this.fbkDetails.name, this.fbkDetails.email, localStorage.getItem('fbkID'));
+					});
+				} else {
+					const message = `Facebook account is not configured`;
+					this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
+				}
+			});
+		} else {
+			const message = `Configuration not available. `;
+			this.layoutUtilsService.showActionNotification(message, MessageType.Create, 4000, true, true);
+		}
 	}
 
 	initTwitterr() {
-		this.setupSocials.forEach(social => {
-			if (social.type === 'twitter') {
-				let url = window.location.href;
-				window.open(this.validURL, '_blank');
-				console.log('callback url', url);
-				this.socialsService.getSocial(url).subscribe(
-					data => {
-						this.loadingSubject.next(false);
-						console.log('success reponse', data);
-						const message = `Success`;
-						localStorage.setItem('registeredTwitter', 'true');
-						this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
-					}, error => {
-						this.loadingSubject.next(false);
-						console.log('Error response', error);
-						const title = 'Please Retry';
-						const message = 'Sorry, Temporary Error Occured';
-						this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
-					});
-			} else {
+		if (this.setupSocials) {
+			if (!this.birdAvailable) {
 				const message = `Twitter account is not configured`;
 				return this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
 			}
-		});
+			this.setupSocials.forEach(social => {
+				if (social && social.type === 'twitter') {
+					console.log('acceoted inner 1');
+					let url = window.location.href;
+					window.open(this.validURL, '_blank');
+					console.log('callback url', url);
+					this.socialsService.getSocial(url).subscribe(
+						data => {
+							this.loadingSubject.next(false);
+							console.log('success reponse', data);
+							const message = `Success`;
+							localStorage.setItem('registeredTwitter', 'true');
+							this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
+						}, error => {
+							this.loadingSubject.next(false);
+							console.log('Error response', error);
+							const title = 'Please Retry';
+							const message = 'Sorry, Temporary Error Occured';
+							this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
+						});
+				} else {
+					console.log('inner invalid');
+					const message = `Twitter account is not configured`;
+					this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
+				}
+			});
+		} else {
+			console.log('returned false here general');
+			const message = `Configuration not available. `;
+			this.layoutUtilsService.showActionNotification(message, MessageType.Create, 4000, true, true);
+		}
 	}
 
-	initLinkedIn()  {
-		this.setupSocials.forEach(social => {
-			if (social.type === 'linkedin') {
-				let url = window.location.href;
-				this.linkedInCode = 'lol';
-				this.linkedInUrl = `http://142.93.6.250/v1/social/linkedin?url=${url}`;
-				window.open(this.linkedInUrl, '_blank');
-				this.socialsService.getSocialLinkedin(url, this.linkedInCode).subscribe(
-					data => {
-						this.loadingSubject.next(false);
-						console.log('success reponse', data);
-						const message = `Success`;
-						this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
-					}, error => {
-						this.loadingSubject.next(false);
-						console.log('Error response', error);
-						const title = 'Please Retry';
-						const message = 'Sorry, Temporary Error Occured';
-						this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
-					});
-			} else {
-				const message = `LinkedIn social network is not configured`;
+	initLinkedIn() {
+		if (this.setupSocials) {
+			if (!this.inAvailable) {
+				const message = `LinkedIn account is not configured`;
 				return this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
 			}
-		});
+			this.setupSocials.forEach(social => {
+				if (social.type === 'linkedin') {
+					let url = window.location.href;
+					this.linkedInCode = 'lol';
+					this.linkedInUrl = `http://142.93.6.250/v1/social/linkedin?url=${url}`;
+					window.open(this.linkedInUrl, '_blank');
+					this.socialsService.getSocialLinkedin(url, this.linkedInCode).subscribe(
+						data => {
+							this.loadingSubject.next(false);
+							console.log('success reponse', data);
+							const message = `Success`;
+							this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
+						}, error => {
+							this.loadingSubject.next(false);
+							console.log('Error response', error);
+							const title = 'Please Retry';
+							const message = 'Sorry, Temporary Error Occured';
+							this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
+						});
+				} else {
+					console.log('linkedin inexistence');
+					const message = `LinkedIn social network is not configured`;
+					this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
+				}
+			});
+		} else {
+			const message = `Configuration not available. `;
+			this.layoutUtilsService.showActionNotification(message, MessageType.Create, 4000, true, true);
+		}
 	}
 
-	initInstagram()  {
-		this.setupSocials.forEach(social => {
-			if (social.type === 'instagram') {
-				let url = window.location.href;
-				this.linkedInCode = 'lol';
-				this.linkedInUrl = `http://142.93.6.250/v1/social/linkedin?url=${url}`;
-				window.open(this.linkedInUrl, '_blank');
-				let payload = {
-					username: this.instaUsername
-				};
-				this.socialsService.getSocialInstagram(payload).subscribe(
-					data => {
-						this.loadingSubject.next(false);
-						console.log('success reponse', data);
-						const message = `Success`;
-						this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
-					}, error => {
-						this.loadingSubject.next(false);
-						console.log('Error response', error);
-						const title = 'Please Retry';
-						const message = 'Sorry, Temporary Error Occured';
-						this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
-					});
-			} else {
-				const message = `Instagram social network is not configured`;
+	initInstagram() {
+		if (this.setupSocials) {
+			if (!this.fbkAvailable) {
+				const message = `Instagram account is not configured`;
 				return this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
 			}
-		});
+			this.setupSocials.forEach(social => {
+				if (social.type === 'instagram') {
+					let url = window.location.href;
+					this.linkedInCode = 'lol';
+					this.linkedInUrl = `http://142.93.6.250/v1/social/linkedin?url=${url}`;
+					window.open(this.linkedInUrl, '_blank');
+					let payload = {
+						username: this.instaUsername
+					};
+					this.socialsService.getSocialInstagram(payload).subscribe(
+						data => {
+							this.loadingSubject.next(false);
+							console.log('success reponse', data);
+							const message = `Success`;
+							this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
+						}, error => {
+							this.loadingSubject.next(false);
+							console.log('Error response', error);
+							const title = 'Please Retry';
+							const message = 'Sorry, Temporary Error Occured';
+							this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
+						});
+				} else {
+					const message = `Instagram social network is not configured`;
+					this.layoutUtilsService.showActionNotification(message, MessageType.Create, 10000, true, true);
+				}
+			});
+		} else {
+			const message = `Configuration not available. `;
+			this.layoutUtilsService.showActionNotification(message, MessageType.Create, 4000, true, true);
+		}
 	}
 
 
