5b7c15268ea78580eda9fe23aa0c471129848aec
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2020-02-03 13:42:31 +0100

    aoi <>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
 	production: true,
 	isMockEnabled: false, // You have to switch this, when your real back-end is done
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
-	BASE_URL: 'http://142.93.6.250/v1',
+	BASE_URL: 'https://api.cdash.com.ng/v1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,5 +8,5 @@ export const environment = {
 	production: false,
 	isMockEnabled: true, // You have to switch this, when your real back-end is done
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
-	BASE_URL: 'http://142.93.6.250/v1',
+	BASE_URL: 'https://api.cdash.com.ng/v1',
 };
